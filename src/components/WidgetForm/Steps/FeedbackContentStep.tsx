413af508c0e896e06fe1f4fc22df92f92a7d89a9
Murilo Thom <murilothom.2002@hotmail.com>
2022-08-11 14:34:07 -0300

    envio do formulario

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,5 +1,5 @@
 import { ArrowLeft } from "phosphor-react";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
 import { ScreenshotButton } from "../ScreenshotButton";
@@ -14,8 +14,18 @@ export function FeedbackContentStep({
   onFeedbackRestartRequested,
 }: FeedbackContentStepProps) {
   const [screenshot, setScreenshot] = useState<string | null>(null);
+  const [comment, setComment] = useState("");
 
   const feedbackTypesInfo = feedbackTypes[feedbackType];
+
+  function handleSubmitFeedback(event: FormEvent) {
+    event.preventDefault();
+
+    console.log({
+      screenshot,
+      comment,
+    });
+  }
 
   return (
     <>
@@ -40,7 +50,7 @@ export function FeedbackContentStep({
         <CloseButton />
       </header>
 
-      <form className="my-4 w-full">
+      <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
         <textarea
           className="min-w-[304px] w-full min-h-[112px] text-sm
           placeholder-zinc-400 text-zinc-100
@@ -50,6 +60,8 @@ export function FeedbackContentStep({
             focus:ring-1 resize-none scrollbar-thumb-zinc-700
             scrollbar-track-transparent scrollbar-thin"
           placeholder="Algo não está funcionando bem?&#10;Queremos corrigir. Conte com detalhes o que está acontecendo..."
+          value={comment}
+          onChange={(event) => setComment(event.target.value)}
         />
 
         <footer className="flex gap-2 mt-2">
@@ -59,8 +71,9 @@ export function FeedbackContentStep({
           />
 
           <button
+            disabled={!comment && true}
             type="submit"
-            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
           >
             Enviar feedback
           </button>
