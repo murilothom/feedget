5ceac1c5c4c34bca1e3c8a92b896ec3735fbd81b
Murilo Thom <murilothom.2002@hotmail.com>
2022-08-11 13:40:40 -0300

    estilização botoes form

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,8 +5,12 @@ module.exports = {
     extend: {
       colors: {
         brand: {
+          300: "#009FE3",
           500: "#0491D4",
         },
+      },
+      borderRadius: {
+        md: "4px",
       },
     },
   },
