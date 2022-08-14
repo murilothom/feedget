f09c0545e82f301c9f5ffff2887e0284759d797e
Murilo Thom <murilothom.2002@hotmail.com>
2022-08-13 21:30:07 -0300

    integrando api criada

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -3,6 +3,7 @@ import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
 import { api } from "../../../lib/api";
 import { CloseButton } from "../../CloseButton";
+import { Loading } from "../Loading";
 import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps {
@@ -18,18 +19,21 @@ export function FeedbackContentStep({
 }: FeedbackContentStepProps) {
   const [screenshot, setScreenshot] = useState<string | null>(null);
   const [comment, setComment] = useState("");
+  const [isSendingFeedback, setIsSendingFeedback] = useState(false);
 
   const feedbackTypesInfo = feedbackTypes[feedbackType];
 
   async function handleSubmitFeedback(event: FormEvent) {
     event.preventDefault();
 
+    setIsSendingFeedback(true);
     await api.post("/feedbacks", {
       type: feedbackType,
       comment,
       screenshot,
     });
 
+    setIsSendingFeedback(false);
     onFeedbackSent();
   }
 
@@ -77,11 +81,11 @@ export function FeedbackContentStep({
           />
 
           <button
-            disabled={!comment && true}
+            disabled={(!comment && true) || isSendingFeedback}
             type="submit"
             className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
           >
-            Enviar feedback
+            {isSendingFeedback ? <Loading /> : <span>Enviar feedback</span>}
           </button>
         </footer>
       </form>
